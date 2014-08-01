2669fe7aa8bdd4913493e2954ca066fd2deb2e58
Justin Manley <manleyjster@gmail.com>
2014-08-01 15:20:18 -0500

    Use Monad Transformers (MaybeT) to handle Maybe + IO monadic results returned from reading in file contents with Data.Aeson.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-{- Run with: ghci -no-user-package-db -package-db .cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d -}
+jiminy research 
+===============
+
+[Easy JSON Parsing with Haskell](http://blog.raynes.me/blog/2012/11/27/easy-json-parsing-in-haskell-with-aeson/)
+
+[Cabal sandbox](http://coldwa.st/e/blog/2013-08-20-Cabal-sandbox.html#future-work)
+
+Run with:
+
+```
+ghci -no-user-package-db -package-db .cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d 
+```
--- analysis/Tracks.hs
+++ analysis/Tracks.hs
@@ -1,19 +1,55 @@
 module Main where
 
 import Data.Maybe (maybeToList)
-import Data.Aeson (decode)
 import Control.Applicative ((<$>))
 import System.Environment (getArgs)
+import Control.Monad (guard)
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class (lift)
+import Data.List (subsequences, isInfixOf, delete)
+
 import BatteryUsage
+
+getSessionUsage :: MaybeT IO [SessionUsage]
+getSessionUsage = do
+	filename <- MaybeT (Just . head <$> getArgs)
+	json <- MaybeT (Just <$> readFile filename)
+	MaybeT . return . decodeString $ json
 
 getNotes :: [SessionUsage] -> [String]
 getNotes = map note
 
+split :: Eq a => a -> [a] -> [[a]]
+split _ [] = [[]]
+split delimiter ls 
+	| not $ [delimiter] `isInfixOf` ls = [ls]
+	| otherwise = first : split delimiter rest where
+		(first, d:rest) = break (== delimiter) ls
 
+---------------------------------------------------------
+-- Handle duration --------------------------------------
+---------------------------------------------------------
+parseDuration :: String -> [Int]
+parseDuration = map read . split ':'
 
---main :: IO (Maybe [SessionUsage])
-main = do 
-	let filename = ((head <$> getArgs) >>= readFile)
-	let notes = decodeString <$> filename 
-	n <- notes
-	fmap ((mapM_ putStrLn) . getNotes) n 
+getDuration :: [SessionUsage] -> [Maybe [Int]]
+getDuration = map (fmap parseDuration . duration)
+
+getHours :: Maybe [Int] -> Maybe Int
+getHours Nothing = Nothing
+getHours (Just x) = Just $ head x
+
+addMaybe :: Num a => Maybe a -> Maybe a -> Maybe a
+addMaybe Nothing b = b
+addMaybe (Just a) b = fmap (+ a) b
+
+getTotalHours :: [Maybe [Int]] -> Maybe Int
+getTotalHours = foldr addMaybe (Just 0) . map getHours
+
+---------------------------------------------------------
+-- main -------------------------------------------------
+---------------------------------------------------------
+main :: IO ()
+main = do
+	Just j <- runMaybeT ((getTotalHours . getDuration) <$> getSessionUsage)
+	print j
